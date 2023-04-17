659668df8f6840f5569ae9fa68d23951698cb340
SIHYUN1212 <130136773+SIHYUN1212@users.noreply.github.com>
2023-04-17 18:44:24 +0900

    cs-3-assignment

--- pages/posts/first-page.js
+++ pages/posts/first-page.js
@@ -1,7 +1,15 @@
+import Head from 'next/head'
 import Link from 'next/link'
+import Script from 'next/script'
+
 export default function FirstPage() {
     return (
+        /* @pjs preload="processing.js","myProgram.pde"; */
         <div>
+            <Head>
+            </Head>
+            <Script src="https://cdnjs.cloudflare.com/ajax/libs/processing.js/1.6.0/processing.min.js"  strategy='beforeInteractive'></Script>
+            <Script type="text/javascript" src="/static/processing.js"  strategy='beforeInteractive'></Script>
             <ol>
                 <h2><li>Name</li></h2>
                 <h3>My name is Si Hyun Jang</h3>
@@ -10,7 +18,10 @@ export default function FirstPage() {
                 <h2><li>Hobby</li></h2>
                 <h3>My hobby is taking pictures, especially landscape photography</h3>
             </ol>
+            <canvas data-processing-sources="/static/myProgram.pde"></canvas>
             <Link href="/">Back to home</Link>
+            
+            
         </div>
     )
 }
